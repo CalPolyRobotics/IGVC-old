208a26683036b2720a4724933c3a157049a3382d
michael1991j <michael1991j@gmail.com>
2014-10-03 18:00:12 -0700

    update library file
    update library file

--- Code/Caddy Board/integration/revA_Alice/Components/Lights/Lights.c
+++ Code/Caddy Board/integration/revA_Alice/Components/Lights/Lights.c
@@ -1,16 +1,9 @@
 //light controller functions
-#include <avr/io.h>
-#include "Lights.h"
 
 //turn lights on and off
 char setLight(char lightTarget) {
    //dummy function, nothing happens
-	//PORTB ^= 0x80;
-	if(lightTarget == 1){
-		PORTB |= 0x80;
-	} else {
-		PORTB &= ~0x80;
-	}
+   //return success
    return 1;
 }
 //specs make no mention of a getLight

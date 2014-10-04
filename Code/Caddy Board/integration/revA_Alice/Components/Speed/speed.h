c77129ec7d9413d6a6f7ce09787b353cf3c8a50d
kkruse <krusek9@gmail.com>
2014-10-03 19:15:06 -0700

    Updated Schematic
    Moved pins in tms570ls0432 schematic library

--- Code/Caddy Board/integration/revA_Alice/Components/Speed/speed.h
+++
@@ -1,8 +0,0 @@
-
-//Speed Sensor group
-
-//get the speed of the vehicle
-char getSpeed(char* sensorResponse);
-
-//will set just the speed of the wheels of the car
-char setSpeed(char speedTarget);
